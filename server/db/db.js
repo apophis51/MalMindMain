2b5f34f1fffa4fab7d8b7cbb0e4f4a28ec9da4b2
apophis51 <malcolmxvernon@hotmail.com>
2023-06-06 10:32:38 -0400

    dsdf

--- server/db/db.js
+++ server/db/db.js
@@ -21,5 +21,5 @@ if(process.env.DATABASE_URL){
 }
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`, config)
+  "postgresql://postgres:s3tVJO1ShNNNdiZyBtLq@containers-us-west-55.railway.app:6736/railway" || `postgres://localhost:5432/${databaseName}`, config)
 module.exports = db
